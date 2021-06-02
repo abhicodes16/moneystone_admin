3d63e07e410e26b3ed0093777935c39b999361c2
ABHI-PC\Abhi Lapsiwala <abhicodes.16@gmail.com>
2021-06-02 12:49:22 +0530

    change url

--- lib/utils/common.dart
+++ lib/utils/common.dart
@@ -1,5 +1,6 @@
 class Common {
   static const String BASE_URL = "https://money-stone.herokuapp.com";
+  //static const String BASE_URL = "https://moneystone-api-kr2n4.ondigitalocean.app";
 
   //API
   static const String USER = BASE_URL + "/user";
